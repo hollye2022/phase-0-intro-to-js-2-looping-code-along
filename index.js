01ebb550d4a695b6c7b3415f2573edb16450fca5
chengying <hollye2558@gmail.com>
2022-10-11 20:13:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+   let message = []
+    for (let i = 0; i < names.length; i++) {
+      message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return message;
+}
+writeCards(names);
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+
+}
